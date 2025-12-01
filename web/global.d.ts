770cd3d9a06a626e8e10436d6269061c3e9d9083
csh781500578 <csh781500578@users.noreply.github.com>
2025-12-01 11:30:04 +0800

    Delete web directory

--- web/global.d.ts
+++
@@ -1,5 +0,0 @@
-declare global {
-  const BuildMode: 'development' | 'production' | 'unknown'
-}
-
-export {}
--- web/src/store/UserStore.ts
+++
@@ -1,6 +0,0 @@
-import { UserInfo } from '@ad/ambase';
-import { proxy } from 'valtio';
-
-export const UserStore = proxy({
-  currentUser: null as null | UserInfo,
-});
